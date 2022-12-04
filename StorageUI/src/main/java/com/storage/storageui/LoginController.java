41e8be24bb1d019928ff942c643942b28e936054
Daniel Zhekov <88737326+daniel-zh23@users.noreply.github.com>
2022-12-04 10:58:40 +0200

    Refactored ui names and created login view.

--- StorageUI/src/main/java/com/storage/storageui/LoginController.java
+++ StorageUI/src/main/java/com/storage/storageui/LoginController.java
@@ -1,11 +1,34 @@
 package com.storage.storageui;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 
 public class LoginController {
 
     @FXML
-    protected void onHelloButtonClick() {
+    private TextField username;
+    @FXML
+    private TextField password;
+    @FXML
+    private Button loginBtn;
+    @FXML
+    private Label errorMsg;
 
+    @FXML
+    protected void onLoginButtonClick() throws Exception{
+        if (username.getText().equals("admin") && password.getText().equals("123456")){
+            FXMLLoader fxmlLoader = new FXMLLoader(StorageApplication.class.getResource("logged-in.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+           Stage window = (Stage) loginBtn.getScene().getWindow();
+           window.setScene(scene);
+        }else{
+            errorMsg.setText("Invalid Login");
+        }
     }
 }
